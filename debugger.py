4a91a266852e5c8cc48092121c2515b6b31a3a00
Sankalp <hackstrix99@gmail.com>
2024-09-14 18:00:19 -0400

    adding debug event listener and CPU thread context

--- debugger.py
+++ debugger.py
@@ -58,11 +58,119 @@ class Debugger():
             )
             print(f"[*] Error: 0x{error_code:08x}. {error_message.value.decode()}")
 
+    def open_process(self,pid):
 
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS,False,pid)
+        return h_process
+    
+    def attach(self,pid):
 
+        self.h_process = self.open_process(pid)
+
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid             = int(pid)
+            
+        else:
+            print("[*] Unable to attach to the process.")
+            error_code = kernel32.GetLastError()
+            print("[*] Error: 0x%08x." % error_code)
+            
+    def run(self):
+        while self.debugger_active == True:
+            # check for any debug events
+            self.get_debug_event()
+
+    def get_debug_event(self):
+
+        debug_event    = DEBUG_EVENT()
+        continue_status= DBG_CONTINUE
+
+        if kernel32.WaitForDebugEvent(byref(debug_event),INFINITE):
+            
+            # obtain the thread and context information for this event
+            print("we got a debug event!")
+        
+            
+
+            input("Press a key to continue...")
+            self.debugger_active = False
+            kernel32.ContinueDebugEvent( \
+                debug_event.dwProcessId, \
+                debug_event.dwThreadId, \
+                continue_status )
+            
+    def detach(self):
+       if kernel32.DebugActiveProcessStop(self.pid):
+            print("[*] Finished debugging. Exiting...")
+            return True
+       else:
+            print("[*] There was an error")
+            error_code = kernel32.GetLastError()
+            print("[*] Error: 0x%08x." % error_code)
+            return False
+
+        
+    def open_thread (self, thread_id):
+
+        h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None,
+            thread_id)
+
+        if h_thread is not None:
+                return h_thread
+        else:
+            print ("[*] Could not obtain a valid thread handle.")
+            return False
+
+    def enumerate_threads(self):
+
+            thread_entry = THREADENTRY32()
+            thread_list  = []
+            snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+
+            if snapshot is not None:
+                # You have to set the size of the struct
+                # or the call will fail
+                thread_entry.dwSize = sizeof(thread_entry)
+                success = kernel32.Thread32First(snapshot,
+                byref(thread_entry))
+
+                while success:
+                    if thread_entry.th32OwnerProcessID == self.pid:
+                        thread_list.append(thread_entry.th32ThreadID)
+                        success = kernel32.Thread32Next(snapshot,
+                        byref(thread_entry))
+
+                kernel32.CloseHandle(snapshot)
+                return thread_list
+            else:
+                return False
+
+    def get_thread_context (self, thread_id=None,h_thread=None):
+
+        context = CONTEXT()
+        context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+
+        # Obtain a handle to the thread
+        if h_thread is None:
+            self.h_thread = self.open_thread(thread_id)
+
+        if kernel32.GetThreadContext(self.h_thread, byref(context)):
+    
+            return context
+        else:
+            return False
+       
 if __name__ == '__main__':
+    # debugger = Debugger()
+    # path_to_exe = r"C:\misc\Python-Debugger\sample-executable\CrucialScan.exe"
+    # # path_to_exe = "./sample-executable/CrucialScan.exe"
+    # print(path_to_exe)
+    # debugger.load(path_to_exe)
+
     debugger = Debugger()
-    path_to_exe = r"C:\misc\Python-Debugger\sample-executable\CrucialScan.exe"
-    # path_to_exe = "./sample-executable/CrucialScan.exe"
-    print(path_to_exe)
-    debugger.load(path_to_exe)
+
+    pid = input("Enter the PID of the process to attach to: ")
+    debugger.attach(int(pid))
+    debugger.run()
+    debugger.detach()
